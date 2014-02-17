e7965e0e2c242f859d9b3519b266948bfe753df2
Zachary Huff <zach.huff.386@gmail.com>
2014-02-16 19:43:10 -0500

    Update bar nodes Improve performance

--- flapbot.js
+++ flapbot.js
@@ -1,189 +1,127 @@
-var drawBars = true;
-var poleX = 115;
-var poleWidth = 180;
-var poleHeight = 512;
-var birdX;
-var birdWidth;
-var birdHeight = 512;
-var modeWidth = 300;
-var modeHeight = 512;
-var birdBar;
-var poleBar;
-var birdInterval;
-var poleInterval;
-var jumpOffset;
-var birdRedS;
-var birdRedE;
-var birdGrnS;
-var birdGrnE;
-var birdBluS;
-var birdBluE;
-var poleTopRed;
-var poleTopGrn;
-var poleTopBlu;
-var poleBotRed;
-var poleBotGrn;
-var poleBotBlu;
-var canvas = document.getElementById('canvas');
-var canvasWell = document.getElementsByClassName('well')[0];
-var ctx = canvas.getContext('2d');
+(function() {
+  'use strict';
+  var drawBars = true;
+  var birdBar;
+  var poleBar;
+  var birdInterval;
+  var poleInterval;
+  var canvas = document.getElementById('canvas');
 
-var msg = document.createElement('div');
-msg.innerText = 'Press "s" to start, "x" to stop';
-msg.setAttribute('style', 'padding: 5px; top: 10px; ' +
-  'left: 10px; background-color: red; position: absolute;');
-document.body.appendChild(msg);
+  var msg = document.createElement('div');
+  msg.innerText = 'Press "s" to start, "x" to stop';
+  msg.setAttribute('style', 'padding: 5px; top: 10px; ' +
+    'left: 10px; background-color: red; position: absolute;');
+  document.body.appendChild(msg);
 
-if (drawBars) {
-  birdBar = document.createElement('span');
-  birdBar.setAttribute('style', 'width: 50px; height: 2px; ' +
-    'margin-top: 3px; margin-left: 76px; background-color: red; ' +
-    'position: absolute;');
-  canvasWell.insertBefore(birdBar, canvas);
+  if (drawBars) {
+    birdBar = document.createElement('span');
+    birdBar.setAttribute('style', 'width: 50px; height: 2px; ' +
+      'margin-top: 3px; margin-left: 76px; background-color: red; ' +
+      'position: absolute;');
+    canvas.parentNode.insertBefore(birdBar, canvas);
 
-  poleBar = document.createElement('span');
-  poleBar.setAttribute('style', 'width: 50px; height: 2px; ' +
-    'margin-top: 3px; margin-left: 136px; background-color: blue; ' +
-    'position: absolute;');
-  canvasWell.insertBefore(poleBar, canvas);
-}
-
-var setupMode = function() {
-  var i;
-  var row;
-  var col;
-  var mode = 'yellow';
-  var data = ctx.getImageData(0, 0, modeWidth, modeHeight).data;
-
-  for (row = 0; row < modeHeight; row++) {
-    for (col = 0; col < modeWidth; col++) {
-      i = (row * modeWidth * 4) + (col * 4);
-      if (data[i] === 122 && data[i + 1] === 198 &&
-          data[i + 2] === 53 && data[i + 3] === 255) {
-        mode = 'green';
-        break;
-      }
-    }
+    poleBar = document.createElement('span');
+    poleBar.setAttribute('style', 'width: 50px; height: 2px; ' +
+      'margin-top: 3px; margin-left: 136px; background-color: blue; ' +
+      'position: absolute;');
+    canvas.parentNode.insertBefore(poleBar, canvas);
   }
 
-  if (mode === 'green') {
-    birdX = 94;
-    birdWidth = 12;
-    jumpOffset = 24;
-    birdRedS = 190;
-    birdRedE = 215;
-    birdGrnS = 40;
-    birdGrnE = 60;
-    birdBluS = 20;
-    birdBluE = 30;
-    poleTopRed = 3;
-    poleTopGrn = 3;
-    poleTopBlu = 3;
-    poleBotRed = 0;
-    poleBotGrn = 117;
-    poleBotBlu = 0;
-  }
-  else {
-    birdX = 96;
-    birdWidth = 8;
-    jumpOffset = 26;
-    birdRedS = 219;
-    birdRedE = 247;
-    birdGrnS = 61;
-    birdGrnE = 85;
-    birdBluS = 0;
-    birdBluE = 0;
-    poleTopRed = 0;
-    poleTopGrn = 0;
-    poleTopBlu = 0;
-    poleBotRed = 173;
-    poleBotGrn = 81;
-    poleBotBlu = 0;
-  }
-};
+  var stopBot = function() {
+    clearInterval(birdInterval);
+    clearInterval(poleInterval);
+  };
 
-var stopBot = function() {
-  clearInterval(birdInterval);
-  clearInterval(poleInterval);
-};
+  var startBot = function() {
+    var pole;
+    var jump;
+    var bird;
+    var birdFound;
+    var birdX = 94;
+    var birdWidth = 12;
+    var birdHeight = 512;
+    var jumpOffset = 25;
+    var poleX = 115;
+    var poleWidth = 180;
+    var poleHeight = 512;
+    var ctx = canvas.getContext('2d');
 
-var startBot = function() {
-  var pole;
-  var jump;
-  var bird;
-  var birdFound;
-
-  stopBot();
-  setupMode();
-
-  birdInterval = setInterval(function() {
-    var i;
-    var row;
-    var col;
-    var data = ctx.getImageData(birdX, 0, birdWidth, birdHeight).data;
-    row = birdFound ? bird - 6 : 0;
-    if (jump) {
-      row -= 9;
-    }
-    birdFound = false;
-
-    for (; row < birdHeight; row++) {
-      for (col = 0; col < birdWidth; col++) {
-        i = (row * birdWidth * 4) + (col * 4);
-        if ((data[i] >= birdRedS && data[i] <= birdRedE) &&
-            (data[i + 1] >= birdGrnS && data[i + 1] <= birdGrnE) &&
-            (data[i + 2] >= birdBluS && data[i + 2] <= birdBluE) &&
-            data[i + 3] === 255) {
-          bird = row;
-          birdFound = true;
-          break;
-        }
-      }
-    }
-
-    if (drawBars) {
-      birdBar.style.marginTop = (3 + bird) + 'px';
-    }
-    if (!jump && bird + jumpOffset > (pole || 256)) {
-      jump = true;
-      setTimeout(function() {
-        jump = false;
-      }, 250);
-      document.body.onkeydown();
-    }
-  }, 4);
-
-  poleInterval = setInterval(function() {
-    var i;
-    var j;
-    var row;
-    var col;
-    var data = ctx.getImageData(poleX, 0, poleWidth, poleHeight).data;
-
-    for (row = 100; row < poleHeight; row++) {
-      for (col = 0; col < poleWidth; col++) {
-        i = (row * poleWidth * 4) + (col * 4);
-        j = ((row + 1) * poleWidth * 4) + (col * 4);
-        if (data[i] === poleTopRed && data[i + 1] === poleTopGrn &&
-            data[i + 2] === poleTopBlu && data[i + 3] === 255 &&
-            data[j] === poleBotRed && data[j + 1] === poleBotGrn &&
-            data[j + 2] === poleBotBlu && data[j + 3] === 255) {
-          pole = row;
-          break;
-        }
-      }
-    }
-    if (drawBars) {
-      poleBar.style.marginTop = (3 + pole) + 'px';
-    }
-  }, 500);
-};
-
-document.body.onkeyup = function(evt) {
-  if (evt.which === 83) {
-    startBot();
-  }
-  else if (evt.which === 88) {
     stopBot();
-  }
-};
+
+    birdInterval = setInterval(function() {
+      var i;
+      var row;
+      var col;
+      var lBirdWidth = birdWidth;
+      var lBirdHeight = birdHeight;
+      var data = ctx.getImageData(birdX, 0, lBirdWidth, lBirdHeight).data;
+
+      row = birdFound ? bird - 6 : 0;
+      if (jump) {
+        row -= 9;
+      }
+      birdFound = false;
+
+      for (; row < lBirdHeight; row++) {
+        for (col = 0; col < lBirdWidth; col++) {
+          i = (row * lBirdWidth * 4) + (col * 4);
+          if ((data[i] >= 190 && data[i] <= 215) &&
+              (data[i + 1] >= 40 && data[i + 1] <= 60) &&
+              (data[i + 2] >= 20 && data[i + 2] <= 30) &&
+              data[i + 3] === 255) {
+            bird = row;
+            birdFound = true;
+            break;
+          }
+        }
+      }
+
+      if (drawBars) {
+        birdBar.style.marginTop = (3 + bird) + 'px';
+      }
+      if (!jump && bird + jumpOffset > (pole || 256)) {
+        jump = true;
+        setTimeout(function() {
+          jump = false;
+        }, 250);
+        document.body.onkeydown();
+      }
+    }, 4);
+
+    poleInterval = setInterval(function() {
+      var i;
+      var j;
+      var row;
+      var col;
+      var lPoleWidth = poleWidth;
+      var lPoleHeight = poleHeight;
+      var data = ctx.getImageData(poleX, 0, lPoleWidth, lPoleHeight).data;
+
+      for (row = 100; row < lPoleHeight; row++) {
+        for (col = 0; col < lPoleWidth; col++) {
+          i = (row * lPoleWidth * 4) + (col * 4);
+          j = ((row + 1) * lPoleWidth * 4) + (col * 4);
+          if (data[i] === 3 && data[i + 1] === 3 &&
+              data[i + 2] === 3 && data[i + 3] === 255 &&
+              data[j] === 0 && data[j + 1] === 117 &&
+              data[j + 2] === 0 && data[j + 3] === 255) {
+            pole = row;
+            break;
+          }
+        }
+      }
+      if (drawBars) {
+        poleBar.style.marginTop = (3 + pole) + 'px';
+      }
+    }, 500);
+  };
+
+  document.body.onkeyup = function(evt) {
+    if (evt.which === 83) {
+      startBot();
+    }
+    else if (evt.which === 88) {
+      stopBot();
+    }
+  };
+}());
